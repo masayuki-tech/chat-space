9787ea5e5d76831c5f4969edea0d6f7c4b49a4f9
masayuki-tech <tennkuuno6@gmail.com>
2020-03-16 14:05:30 +0900

    非同期通信実装

--- Gemfile
+++ Gemfile
@@ -41,7 +41,6 @@ group :development, :test do
   gem 'rails-controller-testing'
   gem 'factory_bot_rails'
   gem 'faker'
-  gem 'capybara'
 end
 
 group :development do
--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,45 @@
 $(function(){
+
+  function buildHTML(message){
+    if (message.image) {
+      var html = 
+        `<div class="message">
+          <div class="user">
+            <div class="user__name">
+              ${message.user_name}
+            </div>
+            <div class="user__at">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__text">
+            <p class="message__text__content">
+              ${message.content}
+            </p>
+            <img src=${message.image} >
+          </div>
+        </div>`   
+      return html;
+    } else {
+      var html = 
+        `<div class="message">
+          <div class="user">
+            <div class="user__name">
+              ${message.user_name}
+            </div>
+            <div class="user__at">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__text">
+            <p class="message__text__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +51,16 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main-contents__chatmessage-list').append(html);
+      $('form')[0].reset();
+      $('.main-contents__chatmessage-list').animate({ scrollTop: $('.main-contents__chatmessage-list')[0].scrollHeight});
+      $('input').prop('disabled', false);
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
     })
   });
 });

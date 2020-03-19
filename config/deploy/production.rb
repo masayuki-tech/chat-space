66c20f905317bd2a4e0deec2a269df548e2b1d0d
masayuki-tech <tennkuuno6@gmail.com>
2020-03-19 15:17:18 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '52.194.1.235', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -60,4 +62,4 @@
 #     # password: "please use keys"
 #   }
 
-server '52.194.1.235', user: 'ec2-user', roles: %w{app db web}
+
